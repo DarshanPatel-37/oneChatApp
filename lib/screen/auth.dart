e3aa71ff33f63943dda4c0236e9e561ce937f68b
DarshanPatel-37 <darshanp2019@gmail.com>
2024-04-15 18:41:06 +0530

    Auth process completed

--- lib/screen/auth.dart
+++ lib/screen/auth.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
+import '../widgets/image_picker.dart';
 
 final _firebase = FirebaseAuth.instance;
 
@@ -12,12 +18,16 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   final _formKey = GlobalKey<FormState>();
+  File? _SelectedImage;
+  final TextEditingController _userName = TextEditingController();
   final TextEditingController _emailaddress = TextEditingController();
   final TextEditingController _password = TextEditingController();
   var _isLogin = true;
+  var _isAuthanticating = false;
+
   Future<void> _submit() async {
     var isValid = _formKey.currentState!.validate();
-    if (!isValid) return;
+    if (!isValid || !_isLogin && _SelectedImage == null) return;
     try {
       if (_isLogin) {
         final UserCredential userCrediantial =
@@ -27,10 +37,29 @@ class _AuthScreenState extends State<AuthScreen> {
         // login state
       } else {
         //signup state
+        setState(() {
+          _isAuthanticating = true;
+        });
         final UserCredential userCrediantial =
             await _firebase.createUserWithEmailAndPassword(
                 email: _emailaddress.text, password: _password.text);
-        print(userCrediantial);
+        final Reference storeRef = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child('${userCrediantial.user!.uid}.jpg');
+        await storeRef.putFile(_SelectedImage!);
+        final imageUploaded = await storeRef.getDownloadURL();
+        print(imageUploaded);
+        print('---------------------------before------------------------');
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCrediantial.user!.uid)
+            .set({
+          'userName': _userName.text,
+          'email': _emailaddress.text,
+          'image_url': imageUploaded,
+        });
+        print('---------------------------after----------------------------');
       }
     } on FirebaseAuthException catch (error) {
       if (error.code == 'email-already-in-use') {
@@ -42,6 +71,9 @@ class _AuthScreenState extends State<AuthScreen> {
           content: Text(error.code),
         ),
       );
+      setState(() {
+        _isAuthanticating = false;
+      });
     }
   }
 
@@ -70,6 +102,27 @@ class _AuthScreenState extends State<AuthScreen> {
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
                           children: [
+                            if (!_isLogin) ...[
+                              UserImagePicker(
+                                onPickedImage: (pickedImage) {
+                                  _SelectedImage = pickedImage;
+                                  print(_SelectedImage);
+                                },
+                              ),
+                              TextFormField(
+                                controller: _userName,
+                                decoration: const InputDecoration(
+                                    labelText: "User Name"),
+                                autocorrect: false,
+                                validator: (value) {
+                                  if (value == null ||
+                                      value.trim().length < 4) {
+                                    return 'Please enter atleast 4 character.';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ],
                             TextFormField(
                               controller: _emailaddress,
                               decoration: const InputDecoration(
@@ -101,24 +154,28 @@ class _AuthScreenState extends State<AuthScreen> {
                             const SizedBox(
                               height: 20,
                             ),
-                            ElevatedButton(
-                              onPressed: _submit,
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: Theme.of(context)
-                                      .colorScheme
-                                      .primaryContainer),
-                              child: Text(_isLogin ? "Login" : "Signup"),
-                            ),
-                            TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  _isLogin = !_isLogin;
-                                });
-                              },
-                              child: Text(_isLogin
-                                  ? 'Create an account'
-                                  : 'I already have an account.'),
-                            ),
+                            if (_isAuthanticating)
+                              const CircularProgressIndicator(),
+                            if (!_isAuthanticating)
+                              ElevatedButton(
+                                onPressed: _submit,
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Theme.of(context)
+                                        .colorScheme
+                                        .primaryContainer),
+                                child: Text(_isLogin ? "Login" : "Signup"),
+                              ),
+                            if (!_isAuthanticating)
+                              TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _isLogin = !_isLogin;
+                                  });
+                                },
+                                child: Text(_isLogin
+                                    ? 'Create an account'
+                                    : 'I already have an account.'),
+                              ),
                           ],
                         )),
                   ),
